7214908e015ed5416052d04b68fb4f158c1d6ea8
Akojede Peter <peter.oluwamayowa.akojede@gmail.com>
2022-03-10 14:07:44 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print the size of various types
+ * main - Entry point
  *
- * Description: prints the size of various types on the computer it is compiled
- * and run on
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
- * Return: Allways 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
