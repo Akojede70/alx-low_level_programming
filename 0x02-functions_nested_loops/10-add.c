d1cc3854676f7c5acd393890f1048630593afc3b
Akojede Peter <peter.oluwamayowa.akojede@gmail.com>
2022-03-16 10:39:11 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
