21848a4d386d012d34a19c147162495fa328e0c2
Peter_Bass <peter.oluwamayowa.akojede@gmail.com>
2022-04-07 22:27:26 +0100

    Malloc aspect in C programming

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,49 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
  *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * Return: ptr.
+ * if new_size == old_size, returns ptr without changes.
+ * if malloc fails, returns NULL.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
+	if (ptr == NULL)
+		ptr = malloc(new_size);
 
-	sout[i] = '\0';
+	if (new_size == old_size)
+		return (ptr);
 
-	return (sout);
+	free(ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
