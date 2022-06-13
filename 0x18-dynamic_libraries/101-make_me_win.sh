92b9eb4fa0a7ee6756055fb0274aa1b317e596cd
Akojede Peter <peter.oluwamayowa.akojede@gmail.com>
2022-06-13 15:01:54 -0700

    dynamic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Akojede70/alx-low_level_programming/tree/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/Akojede70/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
